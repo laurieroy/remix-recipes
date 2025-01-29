58de8392a7830e6b8d1e5688ce0ff5fbba158fba
Laurie Roy <laurieroy.dev@gmail.com>
2025-01-29 13:08:36 -0800

    Update tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,9 @@ import {
 } from "react-router";
 
 import type { Route } from "./+types/root";
-import "./app.css";
+// import styles from "./app.css?url";
+import "./app.css"
+
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -22,6 +24,10 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  // {
+  //   rel: "stylesheet",
+  //   href: "styles",
+  // },
 ];
 
 export function meta({}: Route.MetaArgs) {
