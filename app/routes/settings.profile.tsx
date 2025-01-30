c94eb6b1258d2e1fe4b029e09d30a460bdad0c54
Laurie Roy <laurieroy.dev@gmail.com>
2025-01-29 17:22:59 -0800

    Demonstrate error boundary

--- app/routes/settings.profile.tsx
+++ app/routes/settings.profile.tsx
@@ -1,8 +1,40 @@
+import {
+  useLoaderData,
+  useRouteError,
+  type LoaderFunction,
+} from "react-router";
+
+export const loader: LoaderFunction = () => {
+  return new Response(JSON.stringify({ message: "yo" }), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
 export default function Profile() {
+  const data = useLoaderData();
+
   return (
     <>
       <h1 className="capitalize">Profile</h1>
       <p>This is the settings profile page</p>
+      <p>From loader {data.message}</p>
     </>
   );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (error instanceof Error) {
+    return (
+      <div className="bg-red-300 border-2 border-red-600 rounded-md p-4">
+        <h1 className="">Whoops, something went wrong. </h1>
+        <p>{error.message}</p>
+      </div>
+    );
+  }
+
+  return <div>An unexpected error occurred</div>;
 }
--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -1,4 +1,10 @@
-import { Link, Outlet, useLoaderData, type LoaderFunction } from "react-router";
+import {
+  Link,
+  Outlet,
+  useLoaderData,
+  useRouteError,
+  type LoaderFunction,
+} from "react-router";
 
 export const loader: LoaderFunction = () => {
   return new Response(
@@ -27,4 +33,19 @@ export default function Settings() {
       <Outlet />
     </>
   );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (error instanceof Error) {
+    return (
+      <div className="bg-red-300 border-2 border-red-600 rounded-md p-4">
+        <h1 className="">Whoops, something went wrong. </h1>
+        <p>{error.message}</p>
+      </div>
+    );
+  }
+
+  return <div>An unexpected error occurred</div>;
 }
